84b03c8b32bf5852ac7aba000ba73995c2c6b104
EduardoKaneko <eduardockaneko@gmail.com>
2019-05-14 09:23:30 -0300

    feat: getting variable's names

--- getRequest.py
+++ getRequest.py
@@ -1,8 +1,6 @@
 import requests
 import json
 
-
 r = requests.get("https://www.googleapis.com/analytics/v3/metadata/ga/columns?key=AIzaSyAhQcvVYyfVaLuQdDyLck-ohewCxqAg5uo")
-
 data = r.json()
-print(data("id"))
+print(data)
