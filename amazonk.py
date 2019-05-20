4eee431b8e67a75fb96fca0c6bab21a4b40c9882
EduardoKaneko <eduardockaneko@gmail.com>
2019-05-20 20:25:32 -0300

    feat: s3 connection

---
+++ amazonk.py
@@ -0,0 +1,26 @@
+import os
+import boto3
+import pandas as pd
+import sys
+
+if sys.version_info[0] < 3: 
+    from StringIO import StringIO # Python 2.x
+else:
+    from io import StringIO # Python 3.x
+
+# get your credentials from environment variables
+aws_id = os.environ['AWS_ID']
+aws_secret = os.environ['AWS_SECRET']
+
+client = boto3.client('s3', aws_access_key_id=aws_id,
+        aws_secret_access_key=aws_secret)
+
+bucket_name = 'Amazon S3/airflow-s3/'
+
+
+object_key = 'rdStandarized-16Mai.csv'
+csv_obj = client.get_object(Bucket=bucket_name, Key=object_key)
+body = csv_obj['Body']
+csv_string = body.read().decode('utf-8')
+
+df = pd.read_csv(StringIO(csv_string))
