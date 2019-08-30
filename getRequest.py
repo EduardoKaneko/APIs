4ad58ddb84e6de4532d2a9e2bc894062f9112e02
Edu <41521917+EduardoKaneko@users.noreply.github.com>
2019-08-30 09:56:01 -0300

    delete: sens files

--- getRequest.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-r = requests.get("https://www.googleapis.com/analytics/v3/metadata/ga/columns?key=AIzaSyAhQcvVYyfVaLuQdDyLck-ohewCxqAg5uo")
-data = r.json()
-print(data)
